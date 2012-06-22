4c0f94b1890752916a564cdaae4fe946b343a9d0
jenkins <jenkins@email.com>
2012-06-22 11:54:19 -0400

    commit1

---
+++ test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  test
+//
+//  Created by lala jalal on 12-06-22.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface test : SenTestCase
+
+@end
---
+++ test/test.m
@@ -0,0 +1,32 @@
+//
+//  test.m
+//  test
+//
+//  Created by lala jalal on 12-06-22.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //STFail(@"Unit tests are not implemented yet in test");
+}
+
+@end
